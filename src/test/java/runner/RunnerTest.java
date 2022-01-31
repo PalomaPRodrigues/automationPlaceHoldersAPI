515cf7637494ce0081a9bf74445ec75be1018eb9
Paloma Pedro  Rodrigues <paloma.rodrigues@yaman.com.br>
2022-01-31 10:59:16 -0300

    Refatoração class runner

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -9,15 +9,18 @@ import java.io.IOException;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"json:target/report/cucumber.json"},
-                 features = {"src/test/resources/Features/"},
-                 glue = {"steps"},
-                 tags = "@PlaceHolders")
+        features = {"src/test/resources/Features/"},
+        glue = {"steps"},
+        tags = "@PlaceHolders")
 
 public class RunnerTest {
 
     @AfterClass
     public static void report() throws IOException {
-        if (System.getProperty("os.name").equals("Windows 10"))
         Runtime.getRuntime().exec("cmd.exe /c mvn cluecumber-report:reporting");
+    }
+
+    public static void pipeline() {
+        if (System.getProperty("os.name").equals("Windows 10")) ;
     }
 }
