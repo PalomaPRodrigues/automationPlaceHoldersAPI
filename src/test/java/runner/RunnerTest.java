f6f77e79261f0d95ed981f317e4de62123130076
Paloma Pedro  Rodrigues <paloma.rodrigues@yaman.com.br>
2022-02-01 09:25:24 -0300

    Refatoração 01/02/2022

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"json:target/report/cucumber.json"},
-        features = {"src/test/resources/Features/"},
+        features = {"src/test/resources/features/"},
         glue = {"steps"},
         tags = "@PlaceHolders")
 
@@ -17,7 +17,9 @@ public class RunnerTest {
 
     @AfterClass
     public static void report() throws IOException {
-        if (System.getProperty("os.name").equals("Windows 10"))
-        Runtime.getRuntime().exec("cmd.exe /c mvn cluecumber-report:reporting");
+        if (System.getProperty("os.name").equals("Windows 10")){
+            Runtime.getRuntime().exec("cmd.exe /c mvn cluecumber-report:reporting");
+        }
+
     }
 }
